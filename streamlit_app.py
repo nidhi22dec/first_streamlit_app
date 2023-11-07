36cfd66af9cc42666274be7e255ab2571f81368c
nidhi22dec <149708337+nidhi22dec@users.noreply.github.com>
2023-11-07 12:34:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title('My Parents Dinner Menu')
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('https://learn.snowflake.com/assets/courseware/v1/ebfb9d6559f4e8b3edb15255de776037/asset-v1:snowflake+ESS-DABW+A+type@asset+block/DABW_42.png Breakfast Menu')
+streamlit.text('https://learn.snowflake.com/assets/courseware/v1/ebfb9d6559f4e8b3edb15255de776037/asset-v1:snowflake+ESS-DABW+A+type@asset+block/DABW_42.png Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
